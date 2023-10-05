c49d8fd3729082cfe79dcee714ff54ee8b01626e
GULT\juan espinosa <juan.espinosa@satlock.com>
2023-10-05 17:21:43 -0500

    cambios en agregar

--- crud-basico/src/app/agregar/agregar.component.ts
+++ crud-basico/src/app/agregar/agregar.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Equipo, EquipoService } from '../services/equipo.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
   styleUrls: ['./agregar.component.css']
 })
-export class AgregarComponent {
+export class AgregarComponent implements OnInit {
+
+  equipo:Equipo={
+    id_equipo:'',
+    nombre:'',
+    logo:''
+  };
+  constructor(private EquipoService:EquipoService, private router:Router){}
+
+  ngOnInit(): void {
+    
+  }
+
+  agregar(){
+    delete this.equipo.id_equipo;
+
+    this.EquipoService.addEquipo(this.equipo).subscribe();
+    this.router.navigate(['/inicio']);
+  }
 
 }
